9283bb3757d2f1a8f7e2692f1e45ac7fc9651a52
ena <teankim2@gmail.com>
2026-05-14 23:16:13 +0900

    add player class

--- objects/entities/player/interaction area/interaction_area.gd
+++ objects/entities/player/interaction area/interaction_area.gd
@@ -51,5 +51,3 @@ func _on_body_entered(body: Node2D) -> void:
 
 func _on_body_exited(body: Node2D) -> void:
   _bodies_inside.erase(body)
-
-
--- objects/entities/player/player.gd
+++ objects/entities/player/player.gd
@@ -3,4 +3,17 @@ extends CharacterBody2D
 
 @export var player_data : PlayerData
 @export var interaction_area : InteractionArea
+@export var friction : float = 0.7
 
+
+func _physics_process(_delta: float) -> void:
+  var movement_direction := Vector2.ZERO
+  
+  movement_direction = Input.get_vector("left", "right", "up", "down")
+  
+  movement_direction *= player_data.speed
+
+  velocity += movement_direction
+  velocity *= friction
+  
+  move_and_slide()
